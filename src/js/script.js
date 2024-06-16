3983a6f01cca4598add601548183b91404e8ff3f
Dmytro Klymenko <dimaklim04@gmail.com>
2024-06-16 21:19:51 +0300

    slider favorites

--- src/js/script.js
+++ src/js/script.js
@@ -1,10 +1,42 @@
-(function () {
-  var btn = document.querySelector('.burger-btn');
-  var menu = document.querySelector('.nav-list')
-  
-  btn.addEventListener('click', function(e) {
-    this.classList.toggle('opened');
-    menu.classList.toggle('nav-list--opened');
+window.addEventListener('DOMContentLoaded', () => {
+  const btn = document.querySelector('.burger-btn'),
+    menu = document.querySelector('.nav-list'),
+    inputs = document.querySelectorAll('[data-season]'),
+    books = document.querySelectorAll('.favorites-items');
 
+  btn.addEventListener('click', (e) => {
+    btn.classList.toggle('opened');
+    menu.classList.toggle('nav-list--opened');
+  })
+
+  async function hideContent() {
+    for (const book of books) {
+      book.classList.remove('fadeIn', 'show');
+      book.classList.add('fadeOut');
+    }
+    await new Promise(resolve => setTimeout(resolve, 900));
+    for (const book of books) {
+      book.classList.add('none');
+
+    }
+  }
+  
+
+  function showContent(i = 0) {
+    books[i].classList.add('show', 'fadeIn');
+
+    books[i].classList.remove('none', 'fadeOut');
+  };
+  books.forEach((book, num) => {
+    inputs.forEach((input, i) => {
+      input.addEventListener('change', async () => {
+        if (i === num) {
+          await hideContent();
+          showContent(i);
+        }
+      });
+    })
   });
-})();
+
+
+});
