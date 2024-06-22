557461b5d09051bae75d29c5fa836b4e4d20aada
Dima <dimaklim04@gmail.com>
2024-06-23 02:17:25 +0300

    visual i vse

--- src/js/script.js
+++ src/js/script.js
@@ -24,6 +24,7 @@ class Modal {
   }
 }
 
+
 const btn = document.querySelector('.burger-btn'),
   menu = document.querySelector('.nav-list'),
   inputs = document.querySelectorAll('[data-season]'),
@@ -43,8 +44,8 @@ btnsLogIn = document.querySelectorAll('[data-LogIn]'),
   formLog = document.querySelector('#formLog'),
   forms = document.querySelectorAll('form')
 formReg = document.querySelector('#formReg'),
-formCardBody = document.querySelector('.card-body')
-  modalReg = new Modal(document.querySelector('#modalReg')),
+  formCardBody = document.querySelector('.card-body')
+modalReg = new Modal(document.querySelector('#modalReg')),
   modalLogIn = new Modal(document.querySelector('#modalLogIn')),
   modalProf = new Modal(document.querySelector('#modalProfile')),
   modalCard = new Modal(document.querySelector('#modalCard')),
@@ -52,6 +53,7 @@ formCardBody = document.querySelector('.card-body')
   modalCardNumber = document.querySelector('.modal__card-number'),
   modalName = document.querySelector('.modal__name'),
   visitsCounter = document.querySelectorAll('[data-visitCounter]')
+booksCounter = document.querySelectorAll('[data-booksCounter]')
 iconAuth = document.querySelector('.icon__auth'),
   buyForm = document.querySelector('.buy__form'),
   inputsBuyForm = buyForm.querySelectorAll('input'),
@@ -60,9 +62,10 @@ iconAuth = document.querySelector('.icon__auth'),
   pText = document.querySelector('#PText'),
   pNum = document.querySelector('#PNum'),
   titleFindCard = document.querySelector('.find')
-  getCard = document.querySelector('.getcard'),
+getCard = document.querySelector('.getcard'),
   visitProfile = document.querySelector('.visit-profile'),
   countersBlock = document.querySelector('.modal__counters'),
+  booksList = document.querySelector('.modal__books-list'),
   cardCopy = document.querySelector('.modal__card-copy');
 dataStorage = JSON.parse(localStorage.getItem('formData')) || [];
 window.addEventListener('DOMContentLoaded', () => {
@@ -90,12 +93,36 @@ window.addEventListener('DOMContentLoaded', () => {
     input.addEventListener('keyup', checkInputs);
   });
 
+  //buy abonement
+
+  buyForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const foundObj = dataStorage.find(obj => obj.fullName === document.querySelector('.modal__name').innerHTML);
+    if (foundObj) {
+      foundObj['abonement'] = true;
+      localStorage.setItem('formData', JSON.stringify(dataStorage));
+      closeModal();
+      updateBtnAfterBuyAbonement();
+    }
+
+  })
+
+
   // Update buttons in section Favorites after login
   function updateBtn() {
     btnsBuy.forEach((btn) => {
-      btn.setAttribute('data-buy', '');
+      btn.setAttribute('data-buy', '1');
     })
   }
+
+  // Update buttons in section Favorites after buy aboniment
+  function updateBtnAfterBuyAbonement() {
+    btnsBuy.forEach(btn => {
+      btn.setAttribute('data-buy', '2');
+    })
+  }
+
+
 
   function updateDigitalCard() {
     cardInputs.forEach(item => {
@@ -104,7 +131,7 @@ window.addEventListener('DOMContentLoaded', () => {
     pText.textContent = searchItem('fullName');
     pText.classList.remove('none');
     pNum.textContent = searchItem('cardNumber');
-    pNum.classList.remove('none');  
+    pNum.classList.remove('none');
     btnCheck.classList.add('none');
     countersBlock.classList.remove('none');
     titleFindCard.textContent = 'Your Library card';
@@ -114,7 +141,7 @@ window.addEventListener('DOMContentLoaded', () => {
       item.classList.add('add');
     });
     pText.classList.add('none');
-    pNum.classList.add('none');  
+    pNum.classList.add('none');
     btnCheck.classList.remove('none');
     countersBlock.classList.add('none');
     titleFindCard.textContent = 'Find your Library card';
@@ -132,6 +159,7 @@ window.addEventListener('DOMContentLoaded', () => {
     modalName.textContent = searchItem('fullName');
     getCard.classList.add('none');
     visitProfile.classList.remove('none');
+    updateBooks();
   };
   // Get capitalize first letters for profile
   function capitalizeFirstLetters(str) {
@@ -143,6 +171,53 @@ window.addEventListener('DOMContentLoaded', () => {
     menuTitle.textContent = searchItem('cardNumber');
     modalCardNumber.textContent = searchItem('cardNumber');
   };
+
+
+  // update books
+  function updateBooks() {
+    const modalNameElement = document.querySelector('.modal__name');
+    if (!modalNameElement) {
+      console.error('.modal__name element not found');
+      return;
+    }
+    
+    const modalName = modalNameElement.innerHTML;
+    const foundObj = dataStorage.find(obj => obj.fullName === modalName);
+  
+    if (!foundObj) {
+      console.error('Object not found in dataStorage');
+      return;
+    }
+  
+    booksCounter.forEach(e => {
+      console.log('Books Counter:', foundObj.booksCounter);
+      e.innerHTML = foundObj.booksCounter;
+    });
+  
+    for (let i = 0; i < foundObj.books.length; i++) {
+      const book = document.createElement('li');
+      book.textContent = foundObj.books[i];
+      booksList.append(book);
+      
+      const nameBook = foundObj.books[i].substring(0, foundObj.books[i].indexOf(','));
+      console.log('Book Name:', nameBook);
+      
+      const nameBooks = document.querySelectorAll('.name-book');
+      nameBooks.forEach(e => {
+        if (e.innerHTML === nameBook) {
+          const btn = e.parentElement.querySelector('.buy');
+          if (btn) {
+            btn.innerHTML = 'Own';
+            btn.className = 'Own';
+            btn.disabled = true;
+          } else {
+            console.error('.buy button not found');
+          }
+        }
+      });
+    }
+  }
+
 
   //  search initials, counters, toggle icons, update info in icons
   function updateIcon() {
@@ -173,6 +248,8 @@ window.addEventListener('DOMContentLoaded', () => {
   }
 
 
+
+
   // Check login user
   function checkLoginAndLogIn() {
     const dataStorage = JSON.parse(localStorage.getItem('formData')) || [];
@@ -180,6 +257,12 @@ window.addEventListener('DOMContentLoaded', () => {
 
     if (loggedInUser) {
       LogIn();
+      const foundObj = dataStorage.find(obj => obj.abonement === true);
+      if (foundObj) {
+        btnsBuy.forEach(e => {
+          updateBtnAfterBuyAbonement();
+        })
+      }
     }
   }
   // Search item by ke in local storage
@@ -258,19 +341,19 @@ window.addEventListener('DOMContentLoaded', () => {
 
     const matchedObject = dataStorage.find(obj => obj.fullName === name && obj.cardNumber === number);
     if (matchedObject) {
-        const counterP = document.querySelector('[data-visitCounter]');
-        counterP.textContent = matchedObject.counter; 
-        countersBlock.classList.remove('none'); 
-        btnCheck.classList.add('none'); 
+      const counterP = document.querySelector('[data-visitCounter]');
+      counterP.textContent = matchedObject.counter;
+      countersBlock.classList.remove('none');
+      btnCheck.classList.add('none');
 
-        setTimeout(() => {
-            btnCheck.classList.remove('none');
-            countersBlock.classList.add('none'); 
-            formCardBody.reset();
-        }, 10000);
+      setTimeout(() => {
+        btnCheck.classList.remove('none');
+        countersBlock.classList.add('none');
+        formCardBody.reset();
+      }, 10000);
 
     }
-});
+  });
 
 
 
@@ -294,6 +377,36 @@ window.addEventListener('DOMContentLoaded', () => {
   });
 
 
+  function buyBook(event) {
+    const title = event.target.parentElement.querySelector('.name-book').innerHTML;
+    const author = event.target.parentElement.querySelector('.writter').innerHTML.substring(3);
+    const bookName = `${title}, ${author}`;
+    const book = document.createElement('li');
+    book.textContent = bookName;
+    booksList.append(book);
+    const modalName = document.querySelector('.modal__name').innerHTML;
+    const foundObj = dataStorage.find(obj => obj.fullName === modalName);
+    if (foundObj) {
+      if (!foundObj.books) {
+        foundObj.books = [];
+      }
+      if (!foundObj.booksCounter) {
+        foundObj.booksCounter = 0;
+      }
+      foundObj.books.push(bookName);
+      foundObj.booksCounter += 1;
+      booksCounter.forEach(e => {
+        e.innerHTML = foundObj.booksCounter;
+      });
+      localStorage.setItem('formData', JSON.stringify(dataStorage));
+    }
+    event.target.innerHTML = 'Own';
+    event.target.className = 'Own';
+    event.target.disabled = true;
+  }
+
+
+
 
   //toggle Modal 
   document.addEventListener('click', (event) => {
@@ -305,8 +418,10 @@ window.addEventListener('DOMContentLoaded', () => {
     } else if (event.target.matches('[data-Reg]')) {
       modalReg.show();
       modalLogIn.hide();
-    } else if (event.target.matches('[data-buy]')) {
+    } else if (event.target.matches('[data-buy="1"]')) {
       modalCard.show();
+    } else if (event.target.matches('[data-buy="2"]')) {
+      buyBook(event);
     } else if (event.target.matches('.buy')) {
       modalLogIn.show();
     }
